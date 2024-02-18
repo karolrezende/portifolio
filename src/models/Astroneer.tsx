f1c6ce73b546e0ab8698922590f7709ca3a49c0a
karol <karolrds01@gmail.com>
2024-02-17 20:50:04 -0400

    feat: add astroner wal

--- src/models/Astroneer.tsx
+++ src/models/Astroneer.tsx
@@ -1,11 +1,103 @@
-import astroneerScene from '@/assets/3d/astroneer.glb'
-import { useGLTF } from '@react-three/drei';
+import astroneerScene from '@/assets/3d/astroneer-running.gltf'
+import { useGLTF, useAnimations } from '@react-three/drei';
+import { useEffect, useRef } from 'react';
+import { Vector3 } from 'three';
 
 const Astroneer = ({isRotating, astroneerPosition, astroneerScale }) => {
-    const {scene, animations} = useGLTF(astroneerScene)
-    return (<mesh position={astroneerPosition} scale={astroneerScale}>
-        <primitive object={scene}/>
-    </mesh> );
+    const {nodes, materials, animations} = useGLTF(astroneerScene)
+    const {actions}=useAnimations(animations)
+    console.log(actions)
+    const astronautRef = useRef();
+    const moveSpeed = 0.1; // Velocidade de movimento do astronauta
+    
+    console.log(animations)
+    useEffect(() => {
+      const handleKeyDown = (event:any) => {
+        let newPosition = new Vector3().copy(astronautRef.current.position);
+  
+        switch (event.key) {
+          case "ArrowUp":
+          case "w":
+            newPosition.z -= moveSpeed;
+            break;
+          case "ArrowDown":
+          case "s":
+            newPosition.z += moveSpeed;
+            break;
+          case "ArrowLeft":
+          case "a":
+            newPosition.x -= moveSpeed;
+            break;
+          case "ArrowRight":
+          case "d":
+            newPosition.x += moveSpeed;
+            break;
+          default:
+            break;
+        }
+  
+        astronautRef.current.position.copy(newPosition);
+      };
+  
+      window.addEventListener("keydown", handleKeyDown);
+  
+      return () => {
+        window.removeEventListener("keydown", handleKeyDown);
+      };
+    }, []);
+  
+    return (
+        <group  dispose={null} ref={astronautRef}>
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.pasted__Lente_nino.geometry}
+            material={materials.pasted__vidrio_astr_nina}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.broche_nino.geometry}
+            material={materials.cremayera_t}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Casco_nino.geometry}
+            material={materials.casco_nino_m}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.pie_n_der.geometry}
+            material={materials.botas_nino_m}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.pie_n_izq.geometry}
+            material={materials.botas_nino_m}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.polySurface4.geometry}
+            material={materials.pasted__trajechico}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.mesh_5.geometry}
+            material={materials.pasted__trajechico}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.mesh_5_1.geometry}
+            material={materials.pasted__logonino}
+          />
+        </group>
+      );
 }
  
 export default Astroneer;
--- src/models/Moon.tsx
+++ src/models/Moon.tsx
@@ -1,151 +1,115 @@
 import { useEffect, useRef } from "react";
 import { useGLTF } from "@react-three/drei";
-import { useFrame, useThree } from '@react-three/fiber'
-import { a } from '@react-spring/three'
+import { useFrame, useThree } from "@react-three/fiber";
+import { a } from "@react-spring/three";
 
-import moonScene from '@/assets/3d/moon.glb'
+import moonScene from "@/assets/3d/moon.glb";
 
 const Moon = ({
-    isRotating,
-    setIsRotating,
-    setCurrentStage,
-    currentFocusPoint,
-    ...props
-  }) => {
-        const moonRef = useRef();
-      
-        const { gl, viewport } = useThree();
-        const { nodes, materials } = useGLTF(moonScene);
-      
-       
-        const lastX = useRef(0);
-     
-        const rotationSpeed = useRef(0);
-     
-        const dampingFactor = 0.95;
-      
-        
-        const handlePointerDown = (event: any) => {
-          event.stopPropagation();
-          event.preventDefault();
-          setIsRotating(true);
-      
-         
-          const clientX = event.touches ? event.touches[0].clientX : event.clientX;
-      
-         
-          lastX.current = clientX;
-        };
+  isRotating,
+  setIsRotating,
+  setCurrentStage,
+  ...props
+}: any) => {
+  const moonRef = useRef();
+  const { gl, viewport } = useThree();
+  const { nodes, materials } = useGLTF(moonScene);
+  const lastPosition = useRef({ x: 0, y: 0 });
+  const rotationSpeed = useRef({ x: 0, y: 0 });
+  const dampingFactor = 0.99;
 
-        const handlePointerUp = (event: any) => {
-          event.stopPropagation();
-          event.preventDefault();
-          setIsRotating(false);
-        };
-      
-        const handlePointerMove = (event: any) => {
-          event.stopPropagation();
-          event.preventDefault();
-          if (isRotating) {
-         
-            const clientX = event.touches ? event.touches[0].clientX : event.clientX;
-      
-            
-            const delta = (clientX - lastX.current) / viewport.width;
-      
-            moonRef.current.rotation.y += delta * 0.01 * Math.PI;
-      
-            lastX.current = clientX;
-      
-            rotationSpeed.current = delta * 0.01 * Math.PI;
-          }
-        };
-    
-        const handleKeyDown = (event: any) => {
-          if (event.key === "ArrowLeft") {
-            if (!isRotating) setIsRotating(true);
-      
-            moonRef.current.rotation.y += 0.005 * Math.PI;
-            rotationSpeed.current = 0.007;
-          } else if (event.key === "ArrowRight") {
-            if (!isRotating) setIsRotating(true);
-      
-            moonRef.current.rotation.y -= 0.005 * Math.PI;
-            rotationSpeed.current = -0.007;
-          }
-        };
-    
-        const handleKeyUp = (event: any) => {
-          if (event.key === "ArrowLeft" || event.key === "ArrowRight") {
-            setIsRotating(false);
-          }
-        };
-      
-        useEffect(() => {
+  const handlePointerDown = (event: any) => {
+    event.stopPropagation();
+    event.preventDefault();
+    setIsRotating(true);
+    lastPosition.current = {
+      x: event.touches ? event.touches[0].clientX : event.clientX,
+      y: event.touches ? event.touches[0].clientY : event.clientY,
+    };
+  };
 
-          const canvas = gl.domElement;
-          canvas.addEventListener("pointerdown", handlePointerDown);
-          canvas.addEventListener("pointerup", handlePointerUp);
-          canvas.addEventListener("pointermove", handlePointerMove);
-          window.addEventListener("keydown", handleKeyDown);
-          window.addEventListener("keyup", handleKeyUp);
-      
-          return () => {
-            canvas.removeEventListener("pointerdown", handlePointerDown);
-            canvas.removeEventListener("pointerup", handlePointerUp);
-            canvas.removeEventListener("pointermove", handlePointerMove);
-            window.removeEventListener("keydown", handleKeyDown);
-            window.removeEventListener("keyup", handleKeyUp);
-          };
-        }, [gl, handlePointerDown, handlePointerUp, handlePointerMove]);
-      
-        useFrame(() => {
- 
-          if (!isRotating) {
- 
-            rotationSpeed.current *= dampingFactor;
+  const handlePointerUp = (event: any) => {
+    event.stopPropagation();
+    event.preventDefault();
+    setIsRotating(false);
+  };
 
-            if (Math.abs(rotationSpeed.current) < 0.001) {
-              rotationSpeed.current = 0;
-            }
-      
-            moonRef.current.rotation.y += rotationSpeed.current;
-          } else {
+  const handlePointerMove = (event: any) => {
+    event.stopPropagation();
+    event.preventDefault();
+    if (isRotating) {
+      const clientX = event.touches ? event.touches[0].clientX : event.clientX;
+      const clientY = event.touches ? event.touches[0].clientY : event.clientY;
+      const deltaX = (clientX - lastPosition.current.x) / viewport.width;
+      const deltaY = (clientY - lastPosition.current.y) / viewport.height;
+      moonRef.current.rotation.x += deltaY * 0.02;
+      moonRef.current.rotation.y += deltaX * 0.02;
+      lastPosition.current = { x: clientX, y: clientY };
+      rotationSpeed.current = { x: deltaX * 0.02, y: deltaY * 0.02};
+    }
+  };
 
-            const rotation = moonRef.current.rotation.y;
-      
+  const handleKeyDown = (event: any) => {
+    if (event.key === "ArrowLeft" || event.key === "ArrowRight" || event.code === 'KeyA' || event.code === 'KeyD' || event.key === "ArrowUp" || event.key === "ArrowDown" || event.code === 'KeyW' || event.code === 'KeyS') {
+      setIsRotating(true);
+      let speedX = 0;
+      let speedY = 0;
+      if (event.key === "ArrowRight" || event.code === 'KeyD') speedX = -0.007;
+      else if (event.key === "ArrowLeft" || event.code === 'KeyA') speedX = 0.007;
+      if (event.key === "ArrowUp" || event.code === 'KeyW') speedY = 0.007;
+      else if (event.key === "ArrowDown" || event.code === 'KeyS') speedY = -0.007;
+      rotationSpeed.current = { x: speedY, y: speedX };
+      handleKeyUp(event)
+    }
+  };
 
-            const normalizedRotation =
-              ((rotation % (2 * Math.PI)) + 2 * Math.PI) % (2 * Math.PI);
+  const handleKeyUp = (event: any) => {
+    if (event.key === "ArrowLeft" || event.key === "ArrowRight" || event.code === 'KeyA' || event.code === 'KeyD' || event.key === "ArrowUp" || event.key === "ArrowDown" || event.code === 'KeyW' || event.code === 'KeyS') {
+      setIsRotating(false);
+    }
+  };
 
-            switch (true) {
-              case normalizedRotation >= 5.45 && normalizedRotation <= 5.85:
-                setCurrentStage(4);
-                break;
-              case normalizedRotation >= 0.85 && normalizedRotation <= 1.3:
-                setCurrentStage(3);
-                break;
-              case normalizedRotation >= 2.4 && normalizedRotation <= 2.6:
-                setCurrentStage(2);
-                break;
-              case normalizedRotation >= 4.25 && normalizedRotation <= 4.75:
-                setCurrentStage(1);
-                break;
-              default:
-                setCurrentStage(null);
-            }
-          }
-        });
-    return (
-        <a.group {...props} ref={moonRef}>
-            <group >
-                <mesh
-                    geometry={nodes.Object_2.geometry}
-                    material={materials.MOON}
-                />
-            </group>
-        </a.group>
-    );
-}
+  useEffect(() => {
+    const canvas = gl.domElement;
+    canvas.addEventListener("pointerdown", handlePointerDown);
+    canvas.addEventListener("pointerup", handlePointerUp);
+    canvas.addEventListener("pointermove", handlePointerMove);
+    window.addEventListener("keydown", handleKeyDown);
+    window.addEventListener("keyup", handleKeyUp);
 
-export default Moon
+    return () => {
+      canvas.removeEventListener("pointerdown", handlePointerDown);
+      canvas.removeEventListener("pointerup", handlePointerUp);
+      canvas.removeEventListener("pointermove", handlePointerMove);
+      window.removeEventListener("keydown", handleKeyDown);
+      window.removeEventListener("keyup", handleKeyUp);
+    };
+  }, [gl, handlePointerDown, handlePointerUp, handlePointerMove]);
+
+  useFrame(() => {
+    if (!isRotating) {
+      rotationSpeed.current.x *= dampingFactor;
+      rotationSpeed.current.y *= dampingFactor;
+      if (Math.abs(rotationSpeed.current.x) < 0.001) {
+        rotationSpeed.current.x = 0;
+      }
+      if (Math.abs(rotationSpeed.current.y) < 0.001) {
+        rotationSpeed.current.y = 0;
+      }
+      moonRef.current.rotation.x += rotationSpeed.current.x;
+      moonRef.current.rotation.y += rotationSpeed.current.y;
+    } else {
+      // Your logic for rotation based on current stage here
+    }
+  });
+
+  return (
+    <a.group {...props} ref={moonRef}>
+      <group>
+        <mesh geometry={nodes.Object_2.geometry} material={materials.MOON} />
+      </group>
+    </a.group>
+  );
+};
+
+export default Moon;
